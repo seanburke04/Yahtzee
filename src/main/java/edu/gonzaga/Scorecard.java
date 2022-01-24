cbf46718d5a826327c60bfd38de099ef6b9f26c0
seanburke04 <sburke5@zagmail.gonzaga.edu>
2022-01-23 17:03:59 -0800

    Fixed the error that the previous version had

--- src/main/java/edu/gonzaga/Scorecard.java
+++ src/main/java/edu/gonzaga/Scorecard.java
@@ -6,52 +6,51 @@ package edu.gonzaga;
 
 public class Scorecard {
 
-    HandOfDice handToScore = new HandOfDice();
-    public Integer aces;
-    public Integer twos;
-    public Integer threes;
-    public Integer fours;
-    public Integer fives;
-    public Integer sixes;
+    public int aces;
+    public int twos;
+    public int threes;
+    public int fours;
+    public int fives;
+    public int sixes;
 
     public void scoreUpper(HandOfDice hand){
         // Score aces
-        for (Integer i = 0; i < 5; i++){
+        for (int i = 0; i < 5; i++){
             if (hand.fullHand[i] == 1){
                 aces++;
             }
         }
 
         // Score twos
-        for (Integer i = 0; i < 5; i++){
+        for (int i = 0; i < 5; i++){
             if (hand.fullHand[i] == 2){
                 twos++;
             }
         }
 
         // Score threes
-        for (Integer i = 0; i < 5; i++){
+        for (int i = 0; i < 5; i++){
             if (hand.fullHand[i] == 3){
                 threes++;
             }
         }
 
         // Score fours
-        for (Integer i = 0; i < 5; i++){
+        for (int i = 0; i < 5; i++){
             if (hand.fullHand[i] == 4){
                 fours++;
             }
         }
 
         // Score fives
-        for (Integer i = 0; i < 5; i++){
+        for (int i = 0; i < 5; i++){
             if (hand.fullHand[i] == 5){
                 fives++;
             }
         }
 
         // Score sixes
-        for (Integer i = 0; i < 5; i++){
+        for (int i = 0; i < 5; i++){
             if (hand.fullHand[i] == 6){
                 sixes++;
             }

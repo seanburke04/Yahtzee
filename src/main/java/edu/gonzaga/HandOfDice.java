165c75ad24406ae8508cb741abba6b92b906c58e
seanburke04 <sburke5@zagmail.gonzaga.edu>
2022-01-26 15:19:19 -0800

    Added headers to the files

--- src/main/java/edu/gonzaga/HandOfDice.java
+++ src/main/java/edu/gonzaga/HandOfDice.java
@@ -1,3 +1,12 @@
+/**
+ * This program rolls and scores the dice for a game of yahtzee
+ * CPSC 224-02, Spring 2022
+ * HW1 Yahtzee Rolling and Scoring
+ * Source: Most of the Die.java file written by Dr. Crandall
+ *
+ * @author Sean Burke
+ * @version v1.0 1/26/22
+ */
 package edu.gonzaga;
 
 /*

519534a5f869d4e3fc6f461a02c3ca27dce588f1
seanburke04 <sburke5@zagmail.gonzaga.edu>
2022-02-27 21:29:08 -0800

    Fixed some formating

--- src/test/java/edu/gonzaga/GeneralTest.java
+++ src/test/java/edu/gonzaga/GeneralTest.java
@@ -5,8 +5,6 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-
-
 public class GeneralTest {
     @Test
     void alwaysTrue() {

302addefae252779f35e68074e6f5a095a28d7ba
seanburke04 <sburke5@zagmail.gonzaga.edu>
2022-03-03 13:56:32 -0800

    Added a new test file for hand of dice, hand of dice must be fixed first

---
+++ src/test/java/edu/gonzaga/HandOfDiceTest.java
@@ -0,0 +1,12 @@
+package edu.gonzaga;
+
+import org.junit.jupiter.api.Test;
+import java.util.ArrayList;
+import org.junit.jupiter.api.Assertions;
+
+public class HandOfDiceTest {
+    @Test
+    void alwaysTrue() {
+        Assertions.assertTrue(true);
+    }
+}

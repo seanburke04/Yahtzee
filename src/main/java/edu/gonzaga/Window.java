cd6df33ba93a88fffdd8f50a5f6949c4a0870d31
seanburke04 <sburke5@zagmail.gonzaga.edu>
2022-04-01 18:38:11 -0700

    Added basics of a window

---
+++ src/main/java/edu/gonzaga/Window.java
@@ -0,0 +1,42 @@
+package edu.gonzaga;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class Window {
+    //Icon
+    private String workingDirectory = System.getProperty("user.dir");
+    private String iconPath = workingDirectory + "/src/media/Logos/dicecup.jpg";
+    private ImageIcon yahtzeeIcon = new ImageIcon(iconPath);
+
+    //Frame
+    private JFrame frame = new JFrame("Yahtzee");
+
+    //Menu
+    JMenuBar menuBarWidget = new JMenuBar();
+    JMenu menuFile = new JMenu("File");
+    JMenu menuHelp = new JMenu("Help");
+    JMenuItem menuFileOpen = new JMenuItem("Open");
+    JMenuItem menuFileSaveAs = new JMenuItem("Save as");
+
+    public void makeWindow(){
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setBounds(520, 200, 500, 500);
+
+        menuBarWidget.add(menuFile);
+        menuBarWidget.add(menuHelp);
+
+        menuFile.add(menuFileOpen);
+        menuFile.add(menuFileSaveAs);
+
+        frame.getContentPane().add(BorderLayout.NORTH, menuBarWidget);
+
+        frame.setIconImage(yahtzeeIcon.getImage());
+    }
+
+    public void makeVisible(Boolean visible){
+        frame.setVisible(visible);
+    }
+}

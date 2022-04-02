79c70873384584cfb2554b9282d7160dd8e889d4
seanburke04 <sburke5@zagmail.gonzaga.edu>
2022-04-01 19:20:03 -0700

    DieImages needs to be finished, mostly empty

--- src/main/java/edu/gonzaga/DieImages.java
+++ src/main/java/edu/gonzaga/DieImages.java
@@ -4,4 +4,6 @@ public class DieImages {
     //General path
     private String workingDirectory = System.getProperty("user.dir");
     private String navigate = "/src/media/Dice";
+
+    //Needs to be finished
 }

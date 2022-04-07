fdb36c797ac147a23ee1351a1801a856682acdc4
seanburke04 <sburke5@zagmail.gonzaga.edu>
2022-04-06 22:42:00 -0700

    Added ScorecardView class with notes for future work (no real code yet)

---
+++ src/main/java/edu/gonzaga/ScorecardView.java
@@ -0,0 +1,22 @@
+package edu.gonzaga;
+
+import javax.swing.*;
+
+public class ScorecardView {
+    JPanel buttonPanel = new JPanel();
+    JButton showScore = new JButton("Show Scores");
+    JTextArea scorecardText = new JTextArea();
+
+    //public void getScores()
+
+    private void addButtonToPanel(){
+        buttonPanel.add(showScore);
+    }
+
+    //use public change support
+    //public void updateScorecardText()
+
+    //private void buttonFunctionality()
+
+    //what else?
+}

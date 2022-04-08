809790b28f39863a1c6ff8233bb571ebe8bb2265
seanburke04 <sburke5@zagmail.gonzaga.edu>
2022-04-08 10:19:38 -0700

    Updated notes

--- src/main/java/edu/gonzaga/Game.java
+++ src/main/java/edu/gonzaga/Game.java
@@ -5,6 +5,9 @@ import javax.swing.*;
 public class Game {
     SettingsView userSettings;
 
+    //Game has a window not the other way around
+    //Main needs an instance of game, game needs an instance of Window
+
     void setupUserSettings(){userSettings = new SettingsView();}
 
     public Game(){

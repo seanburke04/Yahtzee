c33db395a412ddc8d458bbe8f27770a9e7a961c0
seanburke04 <sburke5@zagmail.gonzaga.edu>
2022-04-08 17:23:39 -0700

    Added confirm button and removed SettingsView class

--- src/main/java/edu/gonzaga/SettingsView.java
+++ src/main/java/edu/gonzaga/SettingsView.java
@@ -20,141 +20,14 @@ import java.util.ArrayList;
  */
 
 public class SettingsView {
-    JLabel numSidesText = new JLabel("Number of Sides: ");
-    JLabel numDiceText = new JLabel("Number of Dice: ");
-    JLabel numRollsText = new JLabel("Number of Rolls: ");
-    JComboBox numDice = new JComboBox();
-    JComboBox numSides = new JComboBox();
-    JComboBox numRolls = new JComboBox();
-    JPanel numDicePanel = new JPanel();
-    JPanel numSidesPanel = new JPanel();
-    JPanel numRollsPanel = new JPanel();
-    JPanel settingsPanel = new JPanel();
-    JTextArea displayDefaultSettings = new JTextArea();
-    JPanel displayDefaultSettingsPanel = new JPanel();
 
-    static final Integer NUM_SIDES_INDEX = 0;
-    static final Integer NUM_DICE_INDEX = 1;
-    static final Integer NUM_ROLLS_INDEX = 2;
-    static final Integer DEFAULT_SIDES = 6;
-    static final Integer DEFAULT_DICE = 5;
-    static final Integer DEFAULT_ROLLS = 3;
 
-    ArrayList<Integer> userSettings = new ArrayList<>();
 
-    /**
-     * Default constructor for the SettingsView class, initializes default settings and calls other methods
-     */
-    SettingsView(){
-        userSettings.add(DEFAULT_SIDES);
-        userSettings.add(DEFAULT_DICE);
-        userSettings.add(DEFAULT_ROLLS);
 
-        setupComboBoxes();
-        comboBoxListeners();
-        addToSubPanels();
-        addToSettingsPanel();
-        setDisplayText();
-        addToDisplayDefaultSettingsPanel();
-    }
 
-    /**
-     * Getter for userSettings
-     * @return ArrayList<Integer></Integer> userSettings
-     */
-    public ArrayList<Integer> getUserSettings(){return userSettings;}
 
-    /**
-     * Getter for the settingsPanel (passed to Window)
-     * @return JPanel settingsPanel
-     */
-    public JPanel getSettingsPanel(){return settingsPanel;}
 
-    /**
-     * Getter for the settingsTextArea (passed to Window)
-     * @return JPanel displayDefaultSettingsPanel
-     */
-    public JPanel getDisplayDefaultSettingsPanel(){return displayDefaultSettingsPanel;}
 
-    /**
-     * Adds textArea to its respective panel
-     */
-    void addToDisplayDefaultSettingsPanel(){displayDefaultSettingsPanel.add(displayDefaultSettings);}
 
-    /**
-     * Sets the text to be displayed by the text area
-     */
-    void setDisplayText(){
-        displayDefaultSettings.setText("Default Settings:\n");
-        displayDefaultSettings.append("Number of Sides: 6\n");
-        displayDefaultSettings.append("Number of Dice: 5\n");
-        displayDefaultSettings.append("Number of Rolls: 3\n\n");
-        displayDefaultSettings.append("Would you like to play with these settings?");
-    }
 
-    /**
-     * Adds labels and combo boxes to their respective panels
-     */
-    void addToSubPanels(){
-        numSidesPanel.add(numSidesText);
-        numSidesPanel.add(numSides);
-
-        numDicePanel.add(numDiceText);
-        numDicePanel.add(numDice);
-
-        numRollsPanel.add(numRollsText);
-        numRollsPanel.add(numRolls);
-    }
-
-    /**
-     * Adds combo box panels to a single panel to be used by the window
-     */
-    void addToSettingsPanel(){
-        settingsPanel.setLayout(new BoxLayout(settingsPanel, BoxLayout.Y_AXIS));
-        settingsPanel.add(numSidesPanel);
-        settingsPanel.add(numDicePanel);
-        settingsPanel.add(numRollsPanel);
-    }
-
-    /**
-     * Sets up combo box values
-     */
-    void setupComboBoxes(){
-        //Num dice options
-        numDice.addItem(5);
-        numDice.addItem(6);
-        numDice.addItem(7);
-        //Num sides options
-        numSides.addItem(6);
-        numSides.addItem(8);
-        numSides.addItem(12);
-        //Num rolls options
-        for(int i = 3; i < 6; i++){
-            numRolls.addItem(i);
-        }
-    }
-
-    /**
-     * Adds listeners to combo boxes to get input
-     */
-    void comboBoxListeners(){
-        numDice.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                userSettings.set(NUM_DICE_INDEX,(Integer) numDice.getSelectedItem());
-            }
-        });
-        numSides.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                userSettings.set(NUM_SIDES_INDEX, (Integer) numSides.getSelectedItem());
-            }
-        });
-        numRolls.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                userSettings.set(NUM_ROLLS_INDEX, (Integer) numRolls.getSelectedItem());
-            }
-        });
-    }
 }
--- src/main/java/edu/gonzaga/Window.java
+++ src/main/java/edu/gonzaga/Window.java
@@ -12,6 +12,8 @@ package edu.gonzaga;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.ArrayList;
 
 /**
@@ -24,10 +26,32 @@ public class Window {
     ImageIcon yahtzeeIcon = new ImageIcon(iconPath);
     JFrame mainWindow;
 
+    JLabel numSidesText = new JLabel("Number of Sides: ");
+    JLabel numDiceText = new JLabel("Number of Dice: ");
+    JLabel numRollsText = new JLabel("Number of Rolls: ");
+    JComboBox numDice = new JComboBox();
+    JComboBox numSides = new JComboBox();
+    JComboBox numRolls = new JComboBox();
+    JPanel numDicePanel = new JPanel();
+    JPanel numSidesPanel = new JPanel();
+    JPanel numRollsPanel = new JPanel();
+    JPanel settingsPanel = new JPanel();
+    JTextArea displayDefaultSettings = new JTextArea();
+    JPanel displayDefaultSettingsPanel = new JPanel();
+    JButton confirm = new JButton("Confirm");
+    JPanel buttonPanel = new JPanel();
+
+    static final Integer NUM_SIDES_INDEX = 0;
+    static final Integer NUM_DICE_INDEX = 1;
+    static final Integer NUM_ROLLS_INDEX = 2;
+    static final Integer DEFAULT_SIDES = 6;
+    static final Integer DEFAULT_DICE = 5;
+    static final Integer DEFAULT_ROLLS = 3;
+
+    ArrayList<Integer> userSettings = new ArrayList<>();
+
     Hand hand;
     HandView handView;
-
-    SettingsView userSettings;
 
     /**
      * Initializes everything for the main frame
@@ -45,7 +69,7 @@ public class Window {
      * Passes settings to the game class from the SettingsView class
      * @return ArrayList<Integer></Integer> settings
      */
-    public ArrayList<Integer> passSettings(){return userSettings.getUserSettings();}
+    public ArrayList<Integer> passSettings(){return userSettings;}
 
     /**
      * Sets the frame to visible
@@ -71,7 +95,116 @@ public class Window {
     /**
      * Calls SettingsView constructor so SettingsView methods are called within that class
      */
-    void setupUserSettings(){userSettings = new SettingsView();}
+    void setupUserSettings(){
+        userSettings.add(DEFAULT_SIDES);
+        userSettings.add(DEFAULT_DICE);
+        userSettings.add(DEFAULT_ROLLS);
+
+        setupComboBoxes();
+        comboBoxListeners();
+        addToSubPanels();
+        addToSettingsPanel();
+        setDisplayText();
+        addToDisplayDefaultSettingsPanel();
+        addToButtonPanel();
+        buttonFunctionality();
+    }
+
+    /**
+     * Adds listeners to combo boxes to get input
+     */
+    void comboBoxListeners(){
+        numDice.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                userSettings.set(NUM_DICE_INDEX,(Integer) numDice.getSelectedItem());
+            }
+        });
+        numSides.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                userSettings.set(NUM_SIDES_INDEX, (Integer) numSides.getSelectedItem());
+            }
+        });
+        numRolls.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                userSettings.set(NUM_ROLLS_INDEX, (Integer) numRolls.getSelectedItem());
+            }
+        });
+    }
+
+    /**
+     * Adds combo box panels to a single panel to be used by the window
+     */
+    void addToSettingsPanel(){
+        settingsPanel.setLayout(new BoxLayout(settingsPanel, BoxLayout.Y_AXIS));
+        settingsPanel.add(numSidesPanel);
+        settingsPanel.add(numDicePanel);
+        settingsPanel.add(numRollsPanel);
+    }
+
+    /**
+     * Sets up combo box values
+     */
+    void setupComboBoxes(){
+        //Num dice options
+        numDice.addItem(5);
+        numDice.addItem(6);
+        numDice.addItem(7);
+        //Num sides options
+        numSides.addItem(6);
+        numSides.addItem(8);
+        numSides.addItem(12);
+        //Num rolls options
+        for(int i = 3; i < 6; i++){
+            numRolls.addItem(i);
+        }
+    }
+
+    /**
+     * Sets the text to be displayed by the text area
+     */
+    void setDisplayText(){
+        displayDefaultSettings.setText("Default Settings:\n");
+        displayDefaultSettings.append("Number of Sides: 6\n");
+        displayDefaultSettings.append("Number of Dice: 5\n");
+        displayDefaultSettings.append("Number of Rolls: 3\n\n");
+        displayDefaultSettings.append("Enter chosen settings on the right\n");
+        displayDefaultSettings.append("If none are chosen the game will proceed with the default");
+    }
+
+    /**
+     * Adds labels and combo boxes to their respective panels
+     */
+    void addToSubPanels(){
+        numSidesPanel.add(numSidesText);
+        numSidesPanel.add(numSides);
+
+        numDicePanel.add(numDiceText);
+        numDicePanel.add(numDice);
+
+        numRollsPanel.add(numRollsText);
+        numRollsPanel.add(numRolls);
+    }
+
+    /**
+     * Adds textArea to its respective panel
+     */
+    void addToDisplayDefaultSettingsPanel(){displayDefaultSettingsPanel.add(displayDefaultSettings);}
+
+    void addToButtonPanel(){buttonPanel.add(confirm);}
+
+    void buttonFunctionality(){
+        confirm.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                buttonPanel.setVisible(false);
+                displayDefaultSettingsPanel.setVisible(false);
+                settingsPanel.setVisible(false);
+            }
+        });
+    }
 
     /**
      * Removes components from the main frame
@@ -103,8 +236,9 @@ public class Window {
          */
 
         //For Settings window
-        mainWindow.add(BorderLayout.WEST, userSettings.getDisplayDefaultSettingsPanel());
-        mainWindow.add(BorderLayout.EAST, userSettings.getSettingsPanel());
+        mainWindow.add(BorderLayout.WEST, displayDefaultSettingsPanel);
+        mainWindow.add(BorderLayout.EAST, settingsPanel);
+        mainWindow.add(BorderLayout.SOUTH, buttonPanel);
 
         //This makes Crandall's code work
         //mainWindow.add(BorderLayout.EAST, handView.getPanel());
